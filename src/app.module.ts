150f63941fed30bdb805c4394418b633f54ee5ae
Mohd Sakib <mohdsakib@krapton.com>
2024-09-16 18:39:02 +0530

    Initialized mongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Make the config module globally available
+      envFilePath: '.env', // Specify the .env file
+    }),
+    MongooseModule.forRoot(process.env.MONGO_CONNECTION_STRING),
+    , AuthModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
