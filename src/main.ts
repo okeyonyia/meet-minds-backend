c2f48f7c6b9d1b2211da9e4760e8b3caacf1d324
Mohd Sakib <mohdsakib@krapton.com>
2025-01-17 15:10:51 +0530

    vercelfixes

--- src/main.ts
+++ src/main.ts
@@ -1,65 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
-
-const expressApp = express();
-let cachedServer;
-
-async function bootstrapServer() {
-  if (!cachedServer) {
-    const app = await NestFactory.create(
-      AppModule,
-      new ExpressAdapter(expressApp),
-    );
-    app.enableCors({
-      origin: (origin, callback) => {
-        if (
-          !origin ||
-          origin.startsWith('exp://') ||
-          origin === 'http://localhost:19000'
-        ) {
-          return callback(null, true);
-        }
-      },
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-      credentials: true,
-    });
-
-    await app.init();
-    cachedServer = expressApp;
-  }
-  return cachedServer;
-}
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.setGlobalPrefix('api/v1');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
   app.enableCors({
-    origin: (origin, callback) => {
-      if (
-        !origin ||
-        origin.startsWith('exp://') ||
-        origin === 'http://localhost:19000'
-      ) {
-        return callback(null, true);
-      }
-    },
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   });
 
   const port = process.env.PORT || 3000;
   await app.listen(port, '0.0.0.0');
 }
-
-const isVercel = process.env.VERCEL === '1';
-
-if (isVercel) {
-  module.exports = async (req, res) => {
-    const server = await bootstrapServer();
-    return server(req, res);
-  };
-} else {
-  bootstrap().catch((err) => console.error(err));
-}
+bootstrap();
