36430f4ca57ef250428a84e7904e9516641e3a7d
Mohd Sakib <mohdsakib@krapton.com>
2025-01-17 16:06:49 +0530

    fixed

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
+import express from 'express';
 import { ValidationPipe } from '@nestjs/common';
 
 const expressApp = express();
