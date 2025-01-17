d64a2ddd335fb890284feb251029150a38ae49a2
Mohd Sakib <mohdsakib@krapton.com>
2025-01-17 15:29:40 +0530

    fixes

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,42 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
 import { ValidationPipe } from '@nestjs/common';
+
+const expressApp = express();
+let cachedServer;
+
+async function bootstrapServer() {
+  if (!cachedServer) {
+    const app = await NestFactory.create(
+      AppModule,
+      new ExpressAdapter(expressApp),
+    );
+
+    app.useGlobalPipes(
+      new ValidationPipe({
+        transform: true,
+        transformOptions: {
+          enableImplicitConversion: true,
+        },
+      }),
+    );
+
+    app.enableCors({
+      origin: '*',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+      credentials: true,
+    });
+
+    await app.init();
+    cachedServer = expressApp;
+  }
+  return cachedServer;
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.setGlobalPrefix('api/v1');
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -18,10 +49,21 @@ async function bootstrap() {
 
   app.enableCors({
     origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
   });
 
   const port = process.env.PORT || 3000;
   await app.listen(port, '0.0.0.0');
 }
-bootstrap();
+
+const isVercel = process.env.VERCEL === '1';
+
+if (isVercel) {
+  module.exports = async (req, res) => {
+    const server = await bootstrapServer();
+    return server(req, res);
+  };
+} else {
+  bootstrap().catch((err) => console.error(err));
+}
