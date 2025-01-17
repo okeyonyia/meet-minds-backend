b617506a888468c36956a151f4336c584519a97f
Mohd Sakib <mohdsakib@krapton.com>
2025-01-17 15:41:10 +0530

    fixes

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,8 @@ async function bootstrapServer() {
       new ExpressAdapter(expressApp),
     );
 
+    app.setGlobalPrefix('api/v1');
+
     app.useGlobalPipes(
       new ValidationPipe({
         transform: true,
@@ -37,6 +39,8 @@ async function bootstrapServer() {
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api/v1');
 
   app.useGlobalPipes(
     new ValidationPipe({
