f5161d0fd21a6ca5bad1186a3bd753786d9c3cc0
Mohd Sakib <mohdsakib@krapton.com>
2025-02-24 21:28:13 +0530

    fix

--- src/profile/dto/update-profile-status.dto.ts
+++ src/profile/dto/update-profile-status.dto.ts
@@ -1,11 +1,11 @@
-import { IsEnum, IsEmail, IsDateString, IsOptional } from 'class-validator';
+import { IsEnum, IsEmail, IsString } from 'class-validator';
 import { ApprovedByAdminStatus } from '../schema/profile.schema';
 
 export class UpdateProfileStatusDto {
   @IsEmail()
   email: string;
 
-  @IsDateString()
+  @IsString()
   date_of_birth: string; // Should be in ISO format (YYYY-MM-DD)
 
   @IsEnum(ApprovedByAdminStatus)
