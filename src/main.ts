0b0be3d3702446de3ab3244d9e937308b6401722
Mohd Sakib <mohdsakib@krapton.com>
2025-04-23 20:09:15 +0530

    fixing cors

--- src/main.ts
+++ src/main.ts
@@ -1,87 +1,8 @@
-// import { NestFactory } from '@nestjs/core';
-// import { AppModule } from './app.module';
-// import { ExpressAdapter } from '@nestjs/platform-express';
-// import express from 'express';
-// import { ValidationPipe } from '@nestjs/common';
-
-// const expressApp = express();
-// let cachedServer;
-
-// async function bootstrapServer() {
-//   if (!cachedServer) {
-//     const app = await NestFactory.create(
-//       AppModule,
-//       new ExpressAdapter(expressApp),
-//     );
-
-//     app.setGlobalPrefix('api/v1');
-
-//     app.useGlobalPipes(
-//       new ValidationPipe({
-//         transform: true,
-//         transformOptions: {
-//           enableImplicitConversion: true,
-//         },
-//       }),
-//     );
-
-//     app.enableCors({
-//       // origin: '*',
-//       origin: [
-//         'http://localhost:3000',
-//         'https://pademi.events',
-//         'https://www.pademi.events',
-//         'https://www.octinnovations.com',
-//       ],
-//       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-//       credentials: true,
-//     });
-
-//     await app.init();
-//     cachedServer = expressApp;
-//   }
-//   return cachedServer;
-// }
-
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule);
-
-//   app.setGlobalPrefix('api/v1');
-
-//   app.useGlobalPipes(
-//     new ValidationPipe({
-//       transform: true,
-//       transformOptions: {
-//         enableImplicitConversion: true,
-//       },
-//     }),
-//   );
-
-//   app.enableCors({
-//     origin: '*',
-//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-//     credentials: true,
-//   });
-
-//   const port = process.env.PORT || 8001;
-//   await app.listen(port, '0.0.0.0');
-// }
-
-// const isVercel = process.env.VERCEL === '1';
-
-// if (isVercel) {
-//   module.exports = async (req, res) => {
-//     const server = await bootstrapServer();
-//     return server(req, res);
-//   };
-// } else {
-//   bootstrap().catch((err) => console.error(err));
-// }
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import express from 'express';
+import { ValidationPipe } from '@nestjs/common';
 
 const expressApp = express();
 let cachedServer;
@@ -92,23 +13,26 @@ async function bootstrapServer() {
       AppModule,
       new ExpressAdapter(expressApp),
     );
-    app.enableCors({
-      origin: (origin, callback) => {
-        if (!origin) return callback(null, true);
 
-        const allowedOrigins = [
-          'http://localhost:3000',
-          'https://pademi.events',
-          'https://www.pademi.events/',
-          'https://https://www.octinnovations.com',
-          'http://localhost:8000',
-        ];
-        if (allowedOrigins.includes(origin)) {
-          callback(null, true);
-        } else {
-          callback(new Error('Not allowed by CORS'));
-        }
-      },
+    app.setGlobalPrefix('api/v1');
+
+    app.useGlobalPipes(
+      new ValidationPipe({
+        transform: true,
+        transformOptions: {
+          enableImplicitConversion: true,
+        },
+      }),
+    );
+
+    app.enableCors({
+      origin: '*',
+      // origin: [
+      //   'http://localhost:3000',
+      //   'https://pademi.events',
+      //   'https://www.pademi.events',
+      //   'https://www.octinnovations.com',
+      // ],
       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
       credentials: true,
     });
@@ -122,28 +46,24 @@ async function bootstrapServer() {
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
+  app.setGlobalPrefix('api/v1');
 
-      const allowedOrigins = [
-        'http://localhost:3000',
-        'https://pademi.events',
-        'https://www.pademi.events/',
-        'https://https://www.octinnovations.com',
-        'http://localhost:8000',
-      ];
-      if (allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
+  app.enableCors({
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
 
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 8001;
   await app.listen(port, '0.0.0.0');
 }
 
@@ -157,3 +77,83 @@ if (isVercel) {
 } else {
   bootstrap().catch((err) => console.error(err));
 }
+
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import { ExpressAdapter } from '@nestjs/platform-express';
+// import express from 'express';
+
+// const expressApp = express();
+// let cachedServer;
+
+// async function bootstrapServer() {
+//   if (!cachedServer) {
+//     const app = await NestFactory.create(
+//       AppModule,
+//       new ExpressAdapter(expressApp),
+//     );
+//     app.enableCors({
+//       origin: (origin, callback) => {
+//         if (!origin) return callback(null, true);
+
+//         const allowedOrigins = [
+//           'http://localhost:3000',
+//           'https://pademi.events',
+//           'https://www.pademi.events/',
+//           'https://https://www.octinnovations.com',
+//           'http://localhost:8000',
+//         ];
+//         if (allowedOrigins.includes(origin)) {
+//           callback(null, true);
+//         } else {
+//           callback(new Error('Not allowed by CORS'));
+//         }
+//       },
+//       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+//       credentials: true,
+//     });
+
+//     await app.init();
+//     cachedServer = expressApp;
+//   }
+//   return cachedServer;
+// }
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+
+//   app.enableCors({
+//     origin: (origin, callback) => {
+//       if (!origin) return callback(null, true);
+
+//       const allowedOrigins = [
+//         'http://localhost:3000',
+//         'https://pademi.events',
+//         'https://www.pademi.events/',
+//         'https://https://www.octinnovations.com',
+//         'http://localhost:8000',
+//       ];
+//       if (allowedOrigins.includes(origin)) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error('Not allowed by CORS'));
+//       }
+//     },
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+//     credentials: true,
+//   });
+
+//   const port = process.env.PORT || 3000;
+//   await app.listen(port, '0.0.0.0');
+// }
+
+// const isVercel = process.env.VERCEL === '1';
+
+// if (isVercel) {
+//   module.exports = async (req, res) => {
+//     const server = await bootstrapServer();
+//     return server(req, res);
+//   };
+// } else {
+//   bootstrap().catch((err) => console.error(err));
+// }
