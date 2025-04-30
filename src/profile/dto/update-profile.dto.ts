dc6cd8380cf767920208abaddb738d7fd202ce09
Mohd Sakib <mohdsakib@krapton.com>
2025-04-30 19:03:02 +0530

    fixed profile update

--- src/profile/dto/update-profile.dto.ts
+++ src/profile/dto/update-profile.dto.ts
@@ -1,11 +1,12 @@
 import { OmitType, PartialType } from '@nestjs/mapped-types';
 import { CreateProfileDto } from './create-profile.dto';
 import { ApprovedByAdminStatus } from '../schema/profile.schema';
-import { IsEnum } from 'class-validator';
+import { IsEnum, IsOptional } from 'class-validator';
 
 export class UpdateProfileDto extends PartialType(
   OmitType(CreateProfileDto, ['date_of_birth'] as const),
 ) {
   @IsEnum(ApprovedByAdminStatus)
-  event_creation_approval: ApprovedByAdminStatus;
+  @IsOptional()
+  event_creation_approval?: ApprovedByAdminStatus;
 }
