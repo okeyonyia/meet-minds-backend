454469204c10c9925e03ffaac95e38615674ea34
Mohd Sakib <mohdsakib@krapton.com>
2025-07-10 13:16:54 +0530

    fixes

--- src/event/event.service.ts
+++ src/event/event.service.ts
@@ -223,6 +223,7 @@ export class EventService {
           select: 'full_name location profile_pictures',
           options: { slice: { profile_pictures: 1 } },
         })
+        .sort({ createdAt: -1 })
         .exec();
 
       if (!events || events.length === 0) {
